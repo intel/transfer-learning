2d0eef9491a644a9e20e065f2e268b698e4fa01f
Sriragavi95 <110449919+Sriragavi95@users.noreply.github.com>
2023-03-24 05:34:09 +0530

    Added SIMSIAM feature extraction for custom data loader (#226)
    * Adds custom model support for PYT anomaly detection
    
    Signed-off-by: Melanie Buehler <melanie.h.buehler@intel.com>
    
    * Puts PCA into separate function and adds test
    
    * Renames ls_modules to list_layers
    
    Signed-off-by: Melanie Buehler <melanie.h.buehler@intel.com>
    
    * Updated anomaly detection notebook with pca method
    
    * Fix lint and add list_layers to notebook
    
    * Updated for review comments
    
    * Updated mean, std, initial_checkpoints arguments. Fixed notebook error
    
    * Addressed review comments
    
    * Updated documentation for simsiam_dataloader
    
    * Fixed abstract method error due to simsiam_dataloader
    
    * Addressed review comments
    
    * Set initial_checkpoints to None in Anonlay_Detection.ipynb. Updated test_image_anomaly_detection.py
    
    * Added support for TLT dataset. Included test case for simsiam enabled training
    
    * Fixed accuracy for Predict function
    
    * Updated notebook based on review comments
    
    * Fixed style checks in notebook
    
    * Fix for testcase [tests/pytorch_tests/unit/test_models.py::test_resnet50_anomaly_extract_pca]
    
    * Fixes for unit tests
    
    ---------
    
    Signed-off-by: Melanie Buehler <melanie.h.buehler@intel.com>
    Co-authored-by: Melanie Buehler <melanie.h.buehler@intel.com>
    Co-authored-by: Raul Aguilar <raul.aguilar.figueroa@intel.com>

---
+++ tlt/models/image_anomaly_detection/simsiam/builder.py
@@ -0,0 +1,50 @@
+import torch.nn as nn
+
+
+class SimSiam(nn.Module):
+    """
+    Build a SimSiam model.
+    """
+    def __init__(self, base_encoder, dim=2048, pred_dim=512):
+        """
+        dim: feature dimension (default: 2048)
+        pred_dim: hidden dimension of the predictor (default: 512)
+        """
+        super(SimSiam, self).__init__()
+
+        self.encoder = base_encoder
+
+        # build a 3-layer projector
+        prev_dim = self.encoder.fc.weight.shape[1]
+        self.encoder.fc = nn.Sequential(nn.Linear(prev_dim, prev_dim, bias=False),
+                                        nn.BatchNorm1d(prev_dim),
+                                        nn.ReLU(inplace=True),
+                                        nn.Linear(prev_dim, prev_dim, bias=False),
+                                        nn.BatchNorm1d(prev_dim),
+                                        nn.ReLU(inplace=True),
+                                        self.encoder.fc,
+                                        nn.BatchNorm1d(dim, affine=False))
+        self.encoder.fc[6].bias.requires_grad = False
+
+        # build a 2-layer predictor
+        self.predictor = nn.Sequential(nn.Linear(dim, pred_dim, bias=False),
+                                       nn.BatchNorm1d(pred_dim),
+                                       nn.ReLU(inplace=True),
+                                       nn.Linear(pred_dim, dim))
+
+    def forward(self, x1, x2):
+        """
+        Input:
+            x1: first views of images
+            x2: second views of images
+        Output:
+            p1, p2, z1, z2: predictors and targets of the network
+            See Sec. 3 of https://arxiv.org/abs/2011.10566 for detailed notations
+        """
+        z1 = self.encoder(x1)
+        z2 = self.encoder(x2)
+
+        p1 = self.predictor(z1)
+        p2 = self.predictor(z2)
+
+        return p1, p2, z1.detach(), z2.detach()
