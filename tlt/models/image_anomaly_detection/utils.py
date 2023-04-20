bc6de5e0cefc03b2facb597e970ea49414cbbd7c
Sriragavi95 <110449919+Sriragavi95@users.noreply.github.com>
2023-04-20 09:46:39 +0530

    CutPaste Integration (#261)
    * 1. Added CutPaste feature extraction
    2. Updated code based on feedback for simsiam
    
    * Lint fixes
    
    * 1. Addressed review comments 2. implemented change requests from Pratool 3. Added new notebook as per MtWhitney design requirements
    
    * Adding after a few changes for Pratool to test. No review required as this is not the final copy.
    
    * Fixes for anomaly detection
    
    * Lint fixes
    
    * Updated fucntion calls based on feedback
    
    * Fixed documentation, merge conflicts and relocated files
    
    * Updated notebooks/README.md. Fixed unit tests and minor notebook changes.
    
    * Fixed unit test
    
    * Updated documentation
    
    * Skip pca call when auto-generated input contains NaN
    
    Signed-off-by: Melanie Buehler <melanie.h.buehler@intel.com>
    
    * Changed tests to only 1 epoch
    
    Signed-off-by: Melanie Buehler <melanie.h.buehler@intel.com>
    
    ---------
    
    Signed-off-by: Melanie Buehler <melanie.h.buehler@intel.com>
    Co-authored-by: Raul Aguilar <raul.aguilar.figueroa@intel.com>
    Co-authored-by: Melanie Buehler <melanie.h.buehler@intel.com>

--- tlt/models/image_anomaly_detection/utils.py
+++ tlt/models/image_anomaly_detection/utils.py
@@ -73,9 +73,9 @@ class ProgressMeter(object):
         return '[' + fmt + '/' + fmt.format(num_batches) + ']'
 
 
-def _fit(train_data_loader, model, criterion, optimizer, epoch):
+def _fit_simsiam(train_data_loader, model, criterion, optimizer, epoch):
     """
-    Main PyTorch training loop
+    Main PyTorch Simsiam training loop
     """
     print_freq = 1
     batch_time = AverageMeter('Time', ':6.3f')
@@ -88,14 +88,11 @@ def _fit(train_data_loader, model, criterion, optimizer, epoch):
     end = time.time()
 
     for i, (inputs, _) in enumerate(train_data_loader):
-
+        data_time.update(time.time() - end)
         inputs[0] = inputs[0].to('cpu')
         inputs[1] = inputs[1].to('cpu')
-
         p1, p2, z1, z2 = model(x1=inputs[0], x2=inputs[1])
-
         loss = -(criterion(p1, z2).mean() + criterion(p2, z1).mean()) * 0.5
-
         losses.update(loss.item(), inputs[0].size(0))
 
         optimizer.zero_grad()
@@ -104,7 +101,54 @@ def _fit(train_data_loader, model, criterion, optimizer, epoch):
 
         batch_time.update(time.time() - end)
         end = time.time()
-        print(i, print_freq)
+        if i % print_freq == 0:
+            curr_loss = progress.display(i)
+    return curr_loss
+
+
+def _fit_cutpaste(train_data_loader, model, criterion, optimizer, epoch, freeze_resnet, scheduler):
+    """
+    Main PyTorch CutPaste training loop
+    """
+    print_freq = 1
+    batch_time = AverageMeter('Time', ':6.3f')
+    data_time = AverageMeter('Data', ':6.3f')
+    losses = AverageMeter('Loss', ':.4f')
+    progress = ProgressMeter(
+        len(train_data_loader),
+        [batch_time, data_time, losses],
+        prefix="Epoch: [{}]".format(epoch))
+
+    device = 'cpu'
+    if epoch == freeze_resnet:
+        print(epoch)
+        model.unfreeze()
+
+    end = time.time()
+    for i, (data, _) in enumerate(train_data_loader):
+        # measure data loading time
+        data_time.update(time.time() - end)
+        xs = [x.to(device) for x in data]
+        # zero the parameter gradients
+        optimizer.zero_grad()
+        xc = torch.cat(xs, axis=0)
+        embeds, logits = model(xc)
+
+        # calculate label
+        y = torch.arange(len(xs), device=device)
+        y = y.repeat_interleave(xs[0].size(0))
+        loss = criterion(logits, y)
+
+        losses.update(loss.item(), len(data))
+
+        # regulize weights:
+        loss.backward()
+        optimizer.step()
+        batch_time.update(time.time() - end)
+        end = time.time()
+
+        if scheduler is not None:
+            scheduler.step(epoch)
         if i % print_freq == 0:
             curr_loss = progress.display(i)
     return curr_loss
